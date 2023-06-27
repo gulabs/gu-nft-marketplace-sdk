cfda5f5f7b66467550f022bdb8f38240d1bbb826
cuonghx-gutech <cuonghx@gu-tech.com>
2023-06-27 16:14:43 +0700

    feat: export CHAIN_INFO

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,7 @@ export const minNetPriceRatio = 9800;
 export const minNetPriceRatioDeprecated = 9750;
 
 export { addressesByNetwork } from "./addresses";
+export { CHAIN_INFO, isSupportedChain } from "./chains";
 
 export const version = 1;
 export const contractName = "GUNftMarketplaceExchange";
